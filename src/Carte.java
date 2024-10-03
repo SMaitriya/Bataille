7381229e6160778fa5a9687970283f4d00148b18
MAITRIYA SRAMANER <maitriya@outlook.fr>
2024-10-03 23:37:28 +0200

    class carte

--- src/Carte.java
+++ src/Carte.java
@@ -3,7 +3,8 @@ public class Carte {
     private String couleur;
     private int valeur;
     private boolean estSuperieurA;
-
+    private static String[] couleurs = {"Coeur", "Pique", "Carreau", "Trèfle"};
+    private static int[] valeurs = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13};
 
     // Constructeur
     public Carte(String couleur, int valeur){
@@ -17,6 +18,23 @@ public class Carte {
     public String getCouleur() {
         return couleur;
     }
+
+    public int getValeur() {
+        return valeur;
+    }
+
+    // Modificateurs (Setters)
+    public void setCouleur(String couleur) {
+        this.couleur = couleur;
+    }
+
+    public void setValeur(int valeur) {
+        this.valeur = valeur;
+    }
+
+
+
+    // Comparateur
 
     public boolean estSuperieurA(Carte autrecarte) {
        return  this.valeur > autrecarte.valeur;
--- src/Joueurs.java
+++ src/Joueurs.java
@@ -1,2 +1,26 @@
+import java.util.ArrayList;
+
 public class Joueurs {
+    private String joueurA;
+    private String joueurB;
+    private ArrayList<Carte> cartesmain;
+
+    // constructor
+    public Joueurs(String joueurA, String joueurB) {
+        this.joueurA = joueurA;
+        this.joueurB = joueurB;
+        this.cartesmain = new ArrayList<Carte>();
+    }
+
+    public void tirerCarte(Carte carte) {
+        this.cartesmain.add(carte);
+    }
+
+    public void ajouterCarte(Carte carte, int index) {
+        if (index >= 0 && index <= cartesmain.size()) { // Permet d'ajouter à la fin également
+            cartesmain.add(index, carte);
+        }
+    }
+
+
 }
